e9283bbef38974d4a96637d60896c44440a15a60
Daniel Frédéric <daniel.frederic@epita.fr>
2021-07-15 02:22:40 +0200

    Added source location to logs.

--- arch/x86_64/src/main.c
+++ arch/x86_64/src/main.c
@@ -5,16 +5,6 @@
 #include "thirdparties/stivalemgr.h"
 
 void _start(struct stivale2_struct *stivale2_struct) {
-    /*struct stivale2_struct_tag_terminal *term_str_tag;
-    term_str_tag = stivale2_get_tag(stivale2_struct, STIVALE2_STRUCT_TAG_TERMINAL_ID);
-
-    // Check if the tag was actually found.
-    if (term_str_tag == NULL) {
-        // It wasn't found, just hang...
-        for (;;) {
-            asm ("hlt");
-        }
-    }*/
 
     struct framebuffer_pixel
     {
@@ -44,9 +34,6 @@ void _start(struct stivale2_struct *stivale2_struct) {
             framebuffer[raw_position].blue = 255; // met la couleur à bleu
         }
     }
-    // strprint(to_print);
-    // roprint("[PANIC] Halting.\n");
-    // printf("Hello {d}|{s}!\n", 1, "test");
 
     LOG_ERR("Todo : GDT.");
     LOG_PANIC("Halting...");
