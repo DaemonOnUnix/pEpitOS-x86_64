cbe2c802400bc2cd4f47f2f7a72d37538ded65cf
Daniel Frédéric <daniel.frederic@epita.fr>
2021-07-16 02:03:23 +0200

    Painting framebuffer in white.

--- arch/x86_64/src/main.c
+++ arch/x86_64/src/main.c
@@ -21,17 +21,19 @@ void _start(struct stivale2_struct *stivale2_struct) {
     // string to_print = string("[INFO] booted successfully.\n");
 
     LOG_OK("Booted successfully.")
-    LOG_INFO("Size of Framebuffer : {d} x {d}", framebuffer_tag->framebuffer_width, framebuffer_tag->framebuffer_width);
+    LOG_INFO("Size of Framebuffer : {d} x {d}", framebuffer_tag->framebuffer_width, framebuffer_tag->framebuffer_height);
     LOG_INFO("Framebuffer at {x}", framebuffer_tag->framebuffer_addr);
-    LOG_INFO("Painting it in blue.");
+    LOG_INFO("Painting it in white.");
     
     struct framebuffer_pixel* framebuffer = framebuffer_tag->framebuffer_addr;
     for(size_t x = 0; x < framebuffer_tag->framebuffer_width; x++)
     {
         for(size_t y = 0; y < framebuffer_tag->framebuffer_height; y++)
         {
-            size_t raw_position = x + y*framebuffer_tag->framebuffer_width; // convertit les valeurs x et y en position 'brute' dans la table 
-            framebuffer[raw_position].blue = 255; // met la couleur à bleu
+            size_t raw_position = x + y*framebuffer_tag->framebuffer_width; 
+            framebuffer[raw_position].red = 255;
+            framebuffer[raw_position].blue = 255;
+            framebuffer[raw_position].green = 255;
         }
     }
 
