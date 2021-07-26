91504786c410bf54b99142c07839bc76557563c4
Tanguy Baltazart <tbalta914@gmail.com>
2021-07-26 14:39:44 +0200

    begining of hpet timer

--- arch/x86_64/include/UEFI/RSDT.h
+++ arch/x86_64/include/UEFI/RSDT.h
@@ -36,12 +36,34 @@ typedef struct {
     uint32_t PointersToOtherSDT[];
 } PACKED RSDT;
 
-
 typedef struct{
     ACPISDTHeader h;
     uint32_t lapic_addr;
     uint32_t flags;
 } PACKED MADT;
+
+
+typedef struct{
+    uint8_t address_space_id;
+    uint8_t register_bit_width;
+    uint8_t register_bit_offset;
+    uint8_t reserved;
+    uint64_t address; 
+}PACKED HPET_address;
+
+typedef struct{
+    ACPISDTHeader h;
+    uint8_t revision_id;
+    uint8_t comparator_count : 5;
+    uint8_t counter_size : 1;
+    uint8_t reserved : 1;
+    uint8_t legacy_replacement_irq : 1;
+    uint16_t pci_vendor_id;
+    HPET_address address_base;
+    char hpet_number;
+    uint16_t main_counter_minimum;
+    uint8_t page_protection;
+} PACKED HPET;
 
 typedef enum{
     MADT_LAPIC = 0,
